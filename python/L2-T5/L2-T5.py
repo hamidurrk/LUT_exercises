3f33f2710db5f712076572cebaf1d90f7d6a4909
hamidurrk <hamidurrk@gmail.com>
2024-09-13 20:11:27 +0300

    commented accordingly

--- python/L2-T5/L2-T5.py
+++ python/L2-T5/L2-T5.py
@@ -13,13 +13,17 @@ third_number = float(input("Enter the third number: "))
 
 
 sum = first_number + second_number + third_number
-if(float.is_integer(sum)):
-    sum = int(sum)
-avg = sum / 3
-avg_rounded_to_3_decimal_places = round(avg, 3)
-avg_rounded_to_closest_int = round(avg)
-avg_wo_decimal = int(avg)
 
+# Checking if sum is an integer so that it doesn't print a float in the output (eg. 3.0)
+if(float.is_integer(sum)):                      
+    sum = int(sum)
+
+avg = sum / 3
+avg_rounded_to_3_decimal_places = round(avg, 3) # Rounding to 3 decimal places
+avg_rounded_to_closest_int = round(avg) # Rounding to the closest integer
+avg_wo_decimal = int(avg)  # Just removing the decimal part
+
+# Printing the outputs
 print(f"\nSum of the numbers: {sum}")
 print(f"Average of the numbers (rounded to 3 decimal places): {avg_rounded_to_3_decimal_places}")
 print(f"Average of the numbers (rounded to the closest integer): {avg_rounded_to_closest_int}")
