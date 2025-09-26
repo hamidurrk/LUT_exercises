9c9c721e517b61083ceb761acda820e7326d4081
hamidurrk <hamidurrk@gmail.com>
2025-09-26 13:24:19 +0300

    exercise session code test

---
+++ python/tests/operations.py
@@ -0,0 +1,16 @@
+a=int(input(""))
+b=int(input(""))
+sum_result=a+b
+diff_result=a-b
+prod_result=a*b
+div_result=a/b
+qout_result=a//b
+rem_result=a%b
+pow_result=a**b
+print("Their sum:",sum_result)
+print("Their difference:",diff_result)
+print("Their product:",prod_result)
+print("Their division:",div_result)
+print("Their quotient:",qout_result)
+print("Their remainder:",rem_result)
+print("Their power:",pow_result)
