0687bf1cabe31cf5c106f89340a13800b26b7b8c
hamiudurrk <hamidurrk@gmail.com>
2026-02-17 20:18:58 +0000

    Added Echo Machine program to week 6 exercises

---
+++ c/week6/exercise/C6_T1.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    char *line = NULL;
+    size_t len = 0;
+    ssize_t nread;
+    
+    printf("Welcome to the Echo Machine! (Press Ctrl+D to finish)\n");
+    
+    while (1) {
+        printf("Please enter anything:\n");
+        nread = getline(&line, &len, stdin);
+        
+        if (nread == -1) {
+            // EOF reached (Ctrl+D pressed)
+            break;
+        }
+        
+        // Remove newline character if present
+        if (nread > 0 && line[nread - 1] == '\n') {
+            line[nread - 1] = '\0';
+        }
+        
+        printf("You entered: %s\n\n", line);
+    }
+    
+    // Free allocated memory before exiting
+    free(line);
+    
+    return 0;
+}
